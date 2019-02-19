f3d8080b16c08459cfd806d38cc0d48af1b21c99
dimitrije888 <33106203+dimitrije888@users.noreply.github.com>
2019-02-19 21:20:18 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -183,8 +183,105 @@ $j(function() {
         };
     });
 });
+//isScrolledIntoView
+$j(function() {
+    function isScrolledIntoView($elem) {
+        var docViewTop = $j(window).scrollTop();
+        var docViewBottom = docViewTop + $j(window).height();
+        var elemTop = $elem.offset().top;
+        var elemBottom = elemTop + $elem.height();
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
 
+    function count($this) {
+        var current = parseInt($this.html(), 10);
+        if (isScrolledIntoView($this) && !$this.data("isCounting") && current < $this.data('count')) {
+            $this.html(++current);
+            $this.data("isCounting", true);
+            setTimeout(function() {
+                console.log('y');
+                $this.data("isCounting", false);
+                count($this);
+            }, 20);
+        }
+    }
 
+    $j(".c-section1").each(function() {
+        $j(this).data('count', parseInt($j(this).html(), 10));
+        $j(this).html('0');
+        $j(this).data("isCounting", false);
+    });
+
+    function paintProfile() {
+        $j('#navbar_square_experiences').removeClass("background-color-changed");
+        $j('#navbar_square_abilities').removeClass("background-color-changed");
+        $j('#navbar_square_projects').removeClass("background-color-changed");
+        $j('#navbar_square_review').removeClass("background-color-changed");
+        $j('#navbar_square_contact').removeClass("background-color-changed");
+        $j('#navbar_square_profile').addClass("background-color-changed");
+    }
+
+    function paintExperiences() {
+        $j('#navbar_square_profile').removeClass("background-color-changed");
+        $j('#navbar_square_abilities').removeClass("background-color-changed");
+        $j('#navbar_square_projects').removeClass("background-color-changed");
+        $j('#navbar_square_review').removeClass("background-color-changed");
+        $j('#navbar_square_contact').removeClass("background-color-changed");
+        $j('#navbar_square_experiences').addClass("background-color-changed");
+    }
+
+    function paintProject() {
+        $j('#navbar_square_profile').removeClass("background-color-changed");
+        $j('#navbar_square_review').removeClass("background-color-changed");
+        $j('#navbar_square_contact').removeClass("background-color-changed");
+        $j('#navbar_square_experiences').removeClass("background-color-changed");
+        $j('#navbar_square_abilities').removeClass("background-color-changed");
+        $j('#navbar_square_projects').addClass("background-color-changed");
+    }
+
+    function paintAbilities() {
+        $j('#navbar_square_profile').removeClass("background-color-changed");
+        $j('#navbar_square_projects').removeClass("background-color-changed");
+        $j('#navbar_square_review').removeClass("background-color-changed");
+        $j('#navbar_square_contact').removeClass("background-color-changed");
+        $j('#navbar_square_experiences').removeClass("background-color-changed");
+        $j('#navbar_square_abilities').addClass("background-color-changed");
+    }
+
+    function paintReview() {
+        $j('#navbar_square_profile').removeClass("background-color-changed");
+        $j('#navbar_square_contact').removeClass("background-color-changed");
+        $j('#navbar_square_experiences').removeClass("background-color-changed");
+        $j('#navbar_square_abilities').removeClass("background-color-changed");
+        $j('#navbar_square_projects').removeClass("background-color-changed");
+        $j('#navbar_square_review').addClass("background-color-changed");
+    }
+
+    function paintContact() {
+        $j('#navbar_square_profile').removeClass("background-color-changed");
+        $j('#navbar_square_experiences').removeClass("background-color-changed");
+        $j('#navbar_square_abilities').removeClass("background-color-changed");
+        $j('#navbar_square_projects').removeClass("background-color-changed");
+        $j('#navbar_square_review').removeClass("background-color-changed");
+        $j('#navbar_square_contact').addClass("background-color-changed");
+    }
+
+    $j(document).on('scroll', window, function() {
+        if (isScrolledIntoView($j('.section-two-wrapper'))) {
+            paintProfile();
+        } else if (isScrolledIntoView($j('.section-three-wrapper'))) {
+            paintExperiences();
+        } else if (isScrolledIntoView($j('.skills-section-wrapper'))) {
+            paintAbilities();
+        } else if (isScrolledIntoView($j('.section-five-wrapper'))) {
+            paintProject();
+        } else if (isScrolledIntoView($j('.slider-section-wrapper'))) {
+            paintReview();
+        } else if (isScrolledIntoView($j('.footer-section-wrapper'))) {
+            paintContact();
+        }
+    });
+});
 
 //scroll to div
 $j(document).ready(function() {
@@ -194,45 +291,131 @@ $j(document).ready(function() {
         }, 500);
     });
 
+    $j("#navbar_square_up").click(function() {
+        $j('html, body').animate({
+            scrollTop: $j(".section-one-wrapper").offset().top
+        }, 500);
+        /*$j('#navbar_square_experiences').removeClass("background-color-changed");
+        $j('#navbar_square_abilities').removeClass("background-color-changed");
+        $j('#navbar_square_projects').removeClass("background-color-changed");
+        $j('#navbar_square_review').removeClass("background-color-changed");
+        $j('#navbar_square_contact').removeClass("background-color-changed");
+        $j('#navbar_square_profile').removeClass("background-color-changed");*/
+    });
+
     $j("#navbar_square_profile").click(function() {
         $j('html, body').animate({
             scrollTop: $j(".section-two-wrapper").offset().top
         }, 500);
-        $j(this).removeClass("remove-text-underline").addClass("add-text-underline");
+        /*$j('#navbar_square_experiences').removeClass("background-color-changed");
+        $j('#navbar_square_abilities').removeClass("background-color-changed");
+        $j('#navbar_square_projects').removeClass("background-color-changed");
+        $j('#navbar_square_review').removeClass("background-color-changed");
+        $j('#navbar_square_contact').removeClass("background-color-changed");
+        $j('#navbar_square_profile').addClass("background-color-changed");*/
     });
 
     $j("#navbar_square_experiences").click(function() {
         $j('html, body').animate({
             scrollTop: $j(".section-three-wrapper").offset().top
         }, 500);
-        $j(this).removeClass("remove-text-underline").addClass("add-text-underline");
+        /* $j('#navbar_square_profile').removeClass("background-color-changed");
+         $j('#navbar_square_abilities').removeClass("background-color-changed");
+         $j('#navbar_square_projects').removeClass("background-color-changed");
+         $j('#navbar_square_review').removeClass("background-color-changed");
+         $j('#navbar_square_contact').removeClass("background-color-changed");
+         $j('#navbar_square_experiences').addClass("background-color-changed");*/
     });
 
     $j("#navbar_square_abilities").click(function() {
         $j('html, body').animate({
             scrollTop: $j(".section-four-wrapper").offset().top
         }, 500);
-        $j(this).removeClass("remove-text-underline").addClass("add-text-underline");
+        /* $j('#navbar_square_profile').removeClass("background-color-changed");
+         $j('#navbar_square_projects').removeClass("background-color-changed");
+         $j('#navbar_square_review').removeClass("background-color-changed");
+         $j('#navbar_square_contact').removeClass("background-color-changed");
+         $j('#navbar_square_experiences').removeClass("background-color-changed");
+         $j('#navbar_square_abilities').addClass("background-color-changed");*/
     });
 
     $j("#navbar_square_projects").click(function() {
         $j('html, body').animate({
             scrollTop: $j(".section-five-wrapper").offset().top
         }, 500);
-        $j(this).removeClass("remove-text-underline").addClass("add-text-underline");
+        /*   $j('#navbar_square_profile').removeClass("background-color-changed");
+           $j('#navbar_square_review').removeClass("background-color-changed");
+           $j('#navbar_square_contact').removeClass("background-color-changed");
+           $j('#navbar_square_experiences').removeClass("background-color-changed");
+           $j('#navbar_square_abilities').removeClass("background-color-changed");
+           $j('#navbar_square_projects').addClass("background-color-changed");*/
     });
 
     $j("#navbar_square_review").click(function() {
         $j('html, body').animate({
             scrollTop: $j(".section-six-wrapper").offset().top
         }, 500);
-        $j(this).removeClass("remove-text-underline").addClass("add-text-underline");
+        /*   $j('#navbar_square_profile').removeClass("background-color-changed");
+           $j('#navbar_square_contact').removeClass("background-color-changed");
+           $j('#navbar_square_experiences').removeClass("background-color-changed");
+           $j('#navbar_square_abilities').removeClass("background-color-changed");
+           $j('#navbar_square_projects').removeClass("background-color-changed");
+           $j('#navbar_square_review').addClass("background-color-changed");*/
     });
 
     $j("#navbar_square_contact").click(function() {
         $j('html, body').animate({
             scrollTop: $j(".footer-section-wrapper").offset().top
         }, 500);
-        $j(this).removeClass("remove-text-underline").addClass("add-text-underline");
+        /*    $j('#navbar_square_profile').removeClass("background-color-changed");
+            $j('#navbar_square_experiences').removeClass("background-color-changed");
+            $j('#navbar_square_abilities').removeClass("background-color-changed");
+            $j('#navbar_square_projects').removeClass("background-color-changed");
+            $j('#navbar_square_review').removeClass("background-color-changed");
+            $j('#navbar_square_contact').addClass("background-color-changed");*/
     });
+});
+
+$j(document).ready(function() {
+    $j("#language_rs").click(function() {
+        if (i = 2) {
+            var i = 1;
+            $j("#language_rs").animate({
+                right: '20px'
+            }, 200)
+            $j("#language_gb").animate({
+                right: '90px'
+            }, 200)
+        } else {
+            var i = 2;
+            $j("#language_rs").animate({
+                right: '90px'
+            }, 200)
+            $j("#language_gb").animate({
+                right: '20px'
+            }, 200)
+        }
+    })
+});
+
+$j(document).ready(function() {
+    $j("#language_gb").click(function() {
+        if (i = 2) {
+            var i = 1;
+            $j("#language_gb").animate({
+                right: '20px'
+            }, 200)
+            $j("#language_rs").animate({
+                right: '90px'
+            }, 200)
+        } else {
+            var i = 2;
+            $j("#language_gb").animate({
+                right: '90px'
+            }, 200)
+            $j("#language_rs").animate({
+                right: '20px'
+            }, 200)
+        }
+    })
 });
