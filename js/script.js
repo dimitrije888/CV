4b9fb1c1a185afdb46ab89025bcf41f2f2bc24fc
dimitrije888 <33106203+dimitrije888@users.noreply.github.com>
2019-03-13 13:11:34 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -419,3 +419,127 @@ $j(document).ready(function() {
         }
     })
 });
+
+var arrLang = {
+
+
+
+
+
+    'language_gb': {
+        'introduce': 'Let me introduce myself',
+        'profile': 'Profile',
+        'experience': 'Experience',
+        'projects': 'Projects',
+        'abilities': 'Abilities',
+        'review': 'Review',
+        'contact': 'Contact',
+        'profileNaslov': 'Profile',
+        'creative': 'I\'m a creative webprogrammer',
+        'aboutme': 'About me',
+        'aboutmetext': 'I\'m a web programmer with good knowledge of front- end techniques, and im getting better every day.I have excellent skills in communication, individual and team work.Good eyes for detailes, quickly addopt new skills and highly motivated to improve my knowledge.',
+        'details': 'Details',
+        'name': 'Name:',
+        'born': 'Born:',
+        'location': 'Location:',
+        'mesto': 'Leskovac, Serbia, Earth',
+        'experienceNaslov': 'Experience',
+        'goodjudgment': '"Good judgment comes from experience, and a lot of that comes from bad judgments" - Will Rogers',
+        'education': 'Education',
+        'highschool': 'High School',
+        'date1': 'Sep 2008 - July 2012',
+        'highschoolright': 'High school degree - Gymnasium in Leskovac',
+        'hightext': 'I graduate highschool, where I enjoyed doing math and informatic even i was on social studies. I realized in my 4th year of school that i wanted to do something with programming and so i started my education on Faculty of Electronic Engineering in Niš.',
+        'faculty': 'Faculty of Electronic Engineering',
+        'date2': 'Oct 2012 -',
+        'facultyright': 'Computer Science and Informatics',
+        'facultytext': 'I started my education here, and get in touch with various programming languages, but webprogramming draw my attention. After that moment, I have started the education mainly based on webprogramming HTML, CSS, Javascript, frameworks like Bootstrap, Magento, Node.js and javascript library React…',
+        'career': 'Career',
+        'hackathon': 'Hackathon',
+        'date3': '25 May 2018 - 27 May 2018',
+        'hackathonright': 'Hackathon - Code Government',
+        'hackathontext': 'The goal of this Hackathon was to find innovative solutions that will make the portal “eUprava” simpler and more attractive to use. So my team and i signed up for this project. We spent a great time, learned a lot of new things and It was a great experience…',
+        'projectsNaslov': 'Projects',
+        'thejourney': '"The journey of a thousand miles begins with one step." - Lao Tzu',
+        'lotomark': 'Lotomark is a company in Novi Sad involved in SMS-marketing.',
+        'tehnologije': 'Technology:',
+        'miticplast': 'Mitic Plast is firm involved in production and sales products from polypropylene.',
+        'abilitiesNaslov': 'Abilities',
+        'ifopportunity': '"If opportunity doesn\'t knock, build a door." - Milton Berle',
+        'skills': 'Skills',
+        'languagesNaslov': 'Languages',
+        'serbianLanguage': 'Serbian (Mother tongue)',
+        'englishLanguage': 'English',
+        'frenchLanguage': 'French (high school)',
+        'germanLanguage': 'German (Elementary school)',
+        'toolsNaslov': 'Tools',
+        'reviewNaslov': 'Review',
+        'togowrongin': '"The greatest trick the devil ever pulled was convicing the world he didn\'t exist." ― The Usual Suspects, 1995',
+        'contactNaslov': 'Contact',
+        'carpediem': '"Carpe diem. Seize the day, boys. Make your lives extraordinary." - Dead Poets Society, 1989',
+    },
+    'language_rs': {
+        'introduce': 'Dozvolite mi da se predstavim',
+        'profile': 'Profil',
+        'experience': 'Iskustvo',
+        'projects': 'Projekti',
+        'abilities': 'Sposobnosti',
+        'review': 'Recenzija',
+        'contact': 'Kontakt',
+        'profileNaslov': 'Profil',
+        'creative': 'Ja sam kreativan webprogramer',
+        'aboutme': 'O meni',
+        'aboutmetext': 'Ja sam web programer, koji dobro poznaje front-end tehnike, i svakog dana sve vise napredujem. Imam odlične veštine u komunikaciji, kako za samostalan tako i za timski rad. Dobro zapažam detalje, brzo usvajam nova saznanja i veoma sam motivisan da unapredim svoje znanje.',
+        'details': 'Pojedinosti',
+        'name': 'Ime:',
+        'born': 'Rođen:',
+        'location': 'Mesto:',
+        'mesto': 'Leskovac, Srbija, Zemlja',
+        'experienceNaslov': 'Iskustvo',
+        'goodjudgment': '“Dobre procene dolaze sa iskustvom, a iskustvo sa lošim procenama” -  Vil Rodzers',
+        'education': 'Obrazovanje',
+        'highschool': 'Srednja škola',
+        'date1': 'Sep 2008 - Jul 2012',
+        'highschoolright': 'Diploma srednje škole - Gimnazija u Leskovcu',
+        'hightext': 'Završio sam srednju školu, gde su mi omiljeni predmeti bili matematika i informatika, iako sam bio na drušveno-jezičkom smeru. Shvatio sam na IV godini, da želim da se bavim programiranjem, tako sam započeo svoje obrazovanje na Elektronskom fakultetu u Niš-u',
+        'faculty': 'Elektronski Fakultet u Nišu',
+        'date2': 'Okt 2012 -',
+        'facultyright': 'Računarstvo i informatika',
+        'facultytext': 'Krenuo sam svoje obrazovanje ovde i prvi put stupio u kontakt sa mnogim programskim jezicima, ali moju paznju je privuklo web programiranje. Nakon toga sam se bazirao uglavnom na učenje web tehnologija HTML, CSS, Javascript, framework-e kao što su Bootstrap, Magento, Node.js i javascript biblioteke, kao što je React...',
+        'career': 'Karijera',
+        'hackathon': 'Hackathon',
+        'date3': '25 Maj 2018 - 27 Maj 2018',
+        'hackathonright': 'Hackathon - Kodiraj Državu',
+        'hackathontext': 'Cilj ovog Hackatona je bio da se nađe inovativno rešenje za portal "e-uprava", za lakše i atraktivnije za korišćenje. Tako smo se moj tim i ja prijavili za ovaj projekat. Proveli smo sjajne trenutke i naučili dosta na tom Hackathon-u, bilo je to predivno iskustvo...',
+        'projectsNaslov': 'Projekti',
+        'thejourney': '“Putovanje od hiljadu milja počinje prvim korakom” Lao Ce',
+        'lotomark': 'Lotomark je kompanija iz Novog Sada, koja se bavi SMS marketingom',
+        'tehnologije': 'Tehnologije:',
+        'miticplast': 'Mitić Plast je firma koja se bavi proizvodnjom i prodajom proizvoda od polipropilena.',
+        'abilitiesNaslov': 'Sposobnosti',
+        'ifopportunity': '"Ako prilika ne pokuca, izgradite vrata.“ - Milton Berle',
+        'skills' : 'Veštine',
+        'languagesNaslov': 'Jezici',
+        'serbianLanguage': 'Srpski (Maternji jezik)',
+        'englishLanguage': 'Engleski',
+        'frenchLanguage': 'Francuski (Srednja škola)',
+        'germanLanguage': 'Nemački (Osnovna škola)',
+        'toolsNaslov': 'Alati',
+        'reviewNaslov': 'Recenzije',
+        'togowrongin': '"Najveći trik koji je đavo izveo je da ubedi svet da nije ni postojao." - Dežurni krivci, 1995',
+        'contactNaslov': 'Kontakt',
+        'carpediem': '"Carpe diem. Iskoristite dan momci. Učinite vaše živote izvanrednim." - Društvo mrtvih pesnika, 1989',
+    }
+
+};
+
+$j(function (){
+    $j('.language-change').click(function(){
+
+        var lang = $j(this).attr('id');
+
+        $j('.lang').each(function(index, element){
+            $j(this).text(arrLang[lang][$j(this).attr('key')]);
+        });
+    });
+});
